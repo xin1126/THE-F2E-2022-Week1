a843bf3aa8564e64d7e21aff3d2bf596efe8a9a2
xin1126 <z26319491z@gmail.com>
2023-01-14 16:05:28 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import * as path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    AutoImport({
+      imports: [
+        'react',
+        'react-router-dom',
+        {
+          'react-redux': ['useSelector', 'useDispatch'],
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+  },
+})
